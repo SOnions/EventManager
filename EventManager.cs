317b4998b5c95163a210adc4a331c6075b9e4648
SOnions <SOnions@gmail.com>
2016-08-30 13:17:36 +0100

    Uploading the EventManager Files

---
+++ EventManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public delegate void EventDelegate(String eventID, object args);
+
+
+public class EventManager : MonoBehaviour
+{
+    //Member Variables
+    private static EventManager instance;
+    private Dictionary<String, EventDelegate> m_Events = new Dictionary<String, EventDelegate>();
+
+
+    //Initialization
+    void Awake()
+    { 
+        if (instance == null) instance = this;
+    }
+
+
+
+    //Event Handling
+    public static void Subscribe(String eventID, EventDelegate callback)
+    {
+        if (instance != null)
+        {
+            EventDelegate tempDel;
+            if (instance.m_Events.TryGetValue(eventID, out tempDel))
+            {
+                instance.m_Events[eventID] = tempDel += callback;
+            } else
+            {
+                instance.m_Events[eventID] = callback;
+            }
+
+        }
+    }
+
+    public static void Unsubscribe(String eventID, EventDelegate callback)
+    {
+        if (instance != null)
+        {
+            EventDelegate tempDel;
+            if (instance.m_Events.TryGetValue(eventID, out tempDel))
+            {
+                tempDel -= callback;
+                if (tempDel == null)
+                {
+                    instance.m_Events.Remove(eventID);
+                }
+                else {
+                    instance.m_Events[eventID] = tempDel;
+                }
+            } else {
+                Debug.LogWarning("An object tried to unsubscribe frome event " + eventID + " without being subscribed");
+            }
+        }
+    }
+
+    public static void Dispatch(String eventID, object args)
+    {
+        EventDelegate del;
+        if (instance.m_Events.TryGetValue(eventID, out del))
+        {
+            del.Invoke(eventID, args);
+        } else {
+            Debug.LogWarning("Event " + eventID + " was disptched with no listeners");
+        }
+    }
+}
